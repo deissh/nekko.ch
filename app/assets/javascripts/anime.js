4d6ee0c1eccf7643e7f5c746b514d9086147f198
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-08-08 20:10:10 +0300

    refactor(Anime): remove useless code + add show current episode

--- app/assets/javascripts/anime.js
+++ app/assets/javascripts/anime.js
@@ -1,3 +1,26 @@
 $(document).ready(function(){
+    $('.video-button').click(function(e) {
+        var that = $(this);
+        var href = that.data('href');
 
+        $('#player').find('iframe').attr('src', href).parent().show();
+
+        if (!that.hasClass('active')) {
+            var saved = $(this).data('id');
+            var anime_id = $('input[name="page_id"]').val();
+
+            $('.video-button').removeClass('active');
+            that.addClass('active');
+            // $.ajax({
+            //     url: '/saved/' + anime_id + '/' + saved,
+            //     type: 'post',
+            //     success: function(data) {
+            //         if (data.message.length > 0) {
+            //             $('.video-button').removeClass('active');
+            //             that.addClass('active');
+            //         }
+            //     }
+            // });
+        }
+    });
 });
