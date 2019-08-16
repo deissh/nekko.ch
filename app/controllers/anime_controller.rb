a8299e3dedc93a4d0acc839bd7a101883ce1c313
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-08-16 21:34:16 +0300

    feat(anime): rewrite search and add params

--- app/controllers/anime_controller.rb
+++ app/controllers/anime_controller.rb
@@ -1,12 +1,18 @@
 # frozen_string_literal: true
 
 class AnimeController < ApplicationController
-  before_action :anime_load, only: %i[show edit update destroy]
-
   # GET /anime
   # GET /anime.json
   def index
-    @animes = Anime.order(rating: :desc).search_by_title(params[:query])
+    list = Anime
+    list = list.search_by_title(search) if search
+    list = list.limit(limit)
+    list = list.offset(page)
+    list = list.where('genres @> ?', "{#{genres}}") if genres
+    list = list.where(year: year) if year
+    list = list.order(sort_by) if sort_by
+
+    @animes = list
     respond_to do |format|
       format.html { render :index }
       format.json { render json: @animes }
@@ -31,8 +37,47 @@ class AnimeController < ApplicationController
     @anime = Anime.find(params[:id])
   end
 
-  # Never trust parameters from the scary internet, only allow the white list through.
-  def anime_params
-    params.require(:anime).permit(:genres, :status, :year, :rating, :query)
+  def search
+    params[:q]
+  end
+
+  def page
+    params[:page] || 0 * limit
+  end
+
+  def limit
+    params[:limit] || 25
+  end
+
+  def genres
+    params[:genres]
+  end
+
+  def year
+    params[:year]
+  end
+
+  def sort_by
+    case params[:sort_by]
+    when 'rating_desc'
+      return 'rating DESC'
+    when 'rating_asc'
+      return 'rating ASC'
+    when 'year_desc'
+      return 'year DESC'
+    when 'year_asc'
+      return 'year ASC'
+    when 'created_at_desc'
+      return 'created_at DESC'
+    when 'created_at_asc'
+      return 'created_at ASC'
+    when 'updated_at_desc'
+      return 'updated_at DESC'
+    when 'updated_at_asc'
+      return 'updated_at ASC'
+    else
+      # default sort
+      return 'rating DESC'
+    end
   end
 end
