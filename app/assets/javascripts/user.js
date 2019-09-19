102cd944a1721f99aed8f0b22ff8ea5ae162f609
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-09-19 14:27:35 +0300

    feat(user/edit): avatar preview

--- app/assets/javascripts/user.js
+++ app/assets/javascripts/user.js
@@ -5,4 +5,27 @@ $(document).on("turbolinks:load", function(){
     $('#birthdayselect').datetimepicker({
         format: 'L'
     });
+
+    $("#background").change(function () {
+        if (this.files && this.files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#background-preview').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
+    $("#avatar").change(function () {
+        if (this.files && this.files[0]) {
+            const reader = new FileReader();
+
+            reader.onload = function (e) {
+                $('#avatar-preview').attr('src', e.target.result);
+            };
+
+            reader.readAsDataURL(this.files[0]);
+        }
+    });
 });
