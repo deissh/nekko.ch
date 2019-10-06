2168322cc38be65369298222f1955de585acd5b2
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-10-06 11:38:00 +0300

    feat(workers): add sidekiq scheduling

--- config/initializers/sidekiq.rb
+++ config/initializers/sidekiq.rb
@@ -5,3 +5,8 @@ end
 Sidekiq.configure_client do |config|
   config.redis = { url: ENV['REDIS_URL'], network_timeout: 3, pool_timeout: 3 }
 end
+
+schedule_file = "config/schedule.yml"
+if File.exist?(schedule_file) && Sidekiq.server?
+  Sidekiq::Cron::Job.load_from_hash YAML.load_file(schedule_file)
+end
