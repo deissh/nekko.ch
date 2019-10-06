30a9b6f479e3e911f93287b2719ff157d0cc88b2
deissh <deissh@yandex.ru>
2019-10-06 13:57:25 +0300

    Update moonwalk_fetch_all_worker.rb

--- website/app/workers/moonwalk_fetch_all_worker.rb
+++ website/app/workers/moonwalk_fetch_all_worker.rb
@@ -3,7 +3,7 @@ class MoonwalkFetchAllWorker
 
   def perform
     data = JSON.parse(
-      Faraday.get(ENV['MOONWALK_PARSER_URL'] || 'http://localhost:8080' + '/fetch').body
+      Faraday.get((ENV['MOONWALK_PARSER_URL'] || 'http://localhost:8080') + '/fetch').body
     )
 
     data.each do |anime|
