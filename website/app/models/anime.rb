08ce2d7cb4013257a04cd6aee63a9411c8a5739f
Kudryavtsev Kirill <kudryavtsev@nppsatek.ru>
2019-10-06 19:22:55 +0300

    fix(anime): fallback url

--- website/app/models/anime.rb
+++ website/app/models/anime.rb
@@ -38,7 +38,7 @@ class Anime < ApplicationRecord
     if poster.attached?
       Rails.configuration.cdn_baseUrl + poster.attachment.key
     else
-      poster_url unless poster_url.nil? || poster_url == '-'
+      poster_url unless poster_url.nil?
       Rails.configuration.cdn_fallbackUrl
     end
   end
